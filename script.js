46a9c5fcf3e2d7b2e9417a598d79ec5891c83745
Shubham Kumar <shubham2270@gmail.com>
2019-02-10 17:53:07 +0530

    Added sort by image type functionality and catagories on search field

--- script.js
+++ script.js
@@ -10,30 +10,59 @@ const key = "a74a814e-7117-4598-b279-49f6effda03e";
 
 
 
+//Fetched list of catagories with id and name
+const fetchCategory = () => {
+   
+} 
+
+
 
 const displayImages = (e) => {
-    if(e.keyCode === 13) { //checks whether the pressed key is "Enter"
+    if(e.keyCode === 13 || e.type === 'click') { //checks if enter if pressed or click happened
+        image.innerHTML = ""; //Clears the image div.
+        const fileType = document.getElementsByName('fileType'); //Grabs the check boxes
 
-    let searchTerm = searchInput.value;
-
-    fetch(`https://api.thecatapi.com/v1/images/search?limit=3&page=2&order=Desc&mime_types=${searchTerm}`)
-        .then(response => response.json())
+        let imageType = ""; //Stores check boxes checked values
+        
+        fileType.forEach((item) => { //loops through file types
+            if (item.checked == true) {
+                imageType += `,${item.value}`;
+           }
+        }) 
+        
+        fetch(`https://api.thecatapi.com/v1/categories`)
+        .then(res => res.json())
         .then(data => {
-            //Creates img tags and display images
-            data.map((item) => {
-                let imageTag = document.createElement('img');
-                imageTag.src = `${item.url}`;
-                image.appendChild(imageTag); 
+            let searchValue = searchInput.value;
+                let cat =  data.find((categories) => {
+                   return categories.name === searchValue;
+                })
+                fetch(`https://api.thecatapi.com/v1/images/search?limit=6&page=2&order=Desc&mime_types=${imageType}&category_ids=${cat.id}`)
+            .then(response => response.json())
+            .then(data => {
+                //Creates img tags and display images
+                data.map((item) => {
+                    let imageTag = document.createElement('img');
+                    imageTag.src = `${item.url}`;
+                    image.appendChild(imageTag);  //appends the imageTag to image div
+                 })
+             })
             })
-        })
+
+       
+
+        
     }
     
+   
+    wrapper.style.height = '430px';  //moves search bar to top on submit
+    searchInput.style.width = '500px'; //Restrict the search width on click
 
-    //moves search bar to top on submit
-    // container.style.alignContent = 'flex-start';
-    wrapper.style.top = '0px';
+   
 
 }
+
+
 
 //Increase the size of search bar on focus
 const increaseSearchBar = () => {
@@ -47,6 +76,8 @@ const decreaseSearchBar = () => {
 
 
 button.addEventListener('click', displayImages);
+button.addEventListener('click', fetchCategory);
 searchInput.addEventListener('focus', increaseSearchBar);
 searchInput.addEventListener('blur', decreaseSearchBar);
 searchInput.addEventListener('keydown', displayImages);
+
