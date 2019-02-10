12d40a3508e62f91fc0d7677042de8dfde827350
Shubham Kumar <shubham2270@gmail.com>
2019-02-10 19:38:37 +0530

    displays random images if search catagory not available

--- script.js
+++ script.js
@@ -30,36 +30,42 @@ const displayImages = (e) => {
            }
         }) 
         
-        fetch(`https://api.thecatapi.com/v1/categories`)
-        .then(res => res.json())
-        .then(data => {
-            let searchValue = searchInput.value;
-                let cat =  data.find((categories) => {
-                   return categories.name === searchValue;
-                })
-                fetch(`https://api.thecatapi.com/v1/images/search?limit=6&page=2&order=Desc&mime_types=${imageType}&category_ids=${cat.id}`)
-            .then(response => response.json())
+        if (searchInput.value !== null && searchInput.value !== '' && searchInput.value == undefined) { //not blank
+            console.log(searchInput.value)
+            fetch(`https://api.thecatapi.com/v1/categories`)
+            .then(res => res.json())
             .then(data => {
-                //Creates img tags and display images
-                data.map((item) => {
-                    let imageTag = document.createElement('img');
-                    imageTag.src = `${item.url}`;
-                    image.appendChild(imageTag);  //appends the imageTag to image div
+                let searchValue = searchInput.value;
+                    let cat =  data.find((categories) => {
+                       return categories.name === searchValue;
+                    })
+                    fetch(`https://api.thecatapi.com/v1/images/search?limit=6&page=2&order=Desc&mime_types=${imageType}&category_ids=${cat.id}`)
+                .then(response => response.json())
+                .then(data => {
+                    //Creates img tags and display images
+                    data.map((item) => {
+                        let imageTag = document.createElement('img');
+                        imageTag.src = `${item.url}`;
+                        image.appendChild(imageTag);  //appends the imageTag to image div
+                     })
                  })
-             })
-            })
-
+                })
+        } else {
+            fetch(`https://api.thecatapi.com/v1/images/search?limit=6&page=2&order=Desc&mime_types=${imageType}`)
+                .then(response => response.json())
+                .then(data => {
+                    //Creates img tags and display images
+                    data.map((item) => {
+                        let imageTag = document.createElement('img');
+                        imageTag.src = `${item.url}`;
+                        image.appendChild(imageTag);  //appends the imageTag to image div
+                     })
+                 })
+        }
        
-
-        
     }
-    
-   
     wrapper.style.height = '430px';  //moves search bar to top on submit
     searchInput.style.width = '500px'; //Restrict the search width on click
-
-   
-
 }
 
 
